84542a583bd6eb2f5dde366bcf0f988910852993
JuanPabloFernándezAlvarez <juanferfinanzas@gmail.com>
2026-08-03 23:42:21 -0300

    PortfolioFuncionando

--- Infrastructure/Migrations/20260720195318_update ExperienceResponse.Designer.cs
+++ Infrastructure/Migrations/20260720195318_update ExperienceResponse.Designer.cs
@@ -3,6 +3,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(PortfolioDbContext))]
-    partial class PortfolioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260720195318_update ExperienceResponse")]
+    partial class updateExperienceResponse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
